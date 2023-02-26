f945b9883946275705514cbf8591ce856ebe1e4c
Marcos Sanmartín <ahriman89@gmail.com>
2023-02-26 21:15:58 +0100

    Ahora funciona correctamente la alerta si se apaga la base de datos, porque se controla si la variable $conexion es null

--- tarea3/crear.php
+++ tarea3/crear.php
@@ -9,6 +9,7 @@
     $insertado = false;
     $mensajeAlerta = false;
     $error = false;
+    $errorConexionBD = false;
     
     if (isset($_POST['crear']) && isset($_POST['nombre']) && isset($_POST['nombre_corto']) && isset($_POST['precio']) 
         && isset($_POST['familia']) && isset($_POST['descripcion'])) {
@@ -22,29 +23,43 @@
             $consultaSQL = 'INSERT INTO productos (nombre, nombre_corto, pvp, familia, descripcion) 
                                 VALUES (:nombre, :nombre_corto, :precio, :familia, :descripcion)';
                                 
-            try {
-                $stmt = $conexion->prepare($consultaSQL);
-                $stmt->bindParam(":nombre", $nombre);
-                $stmt->bindParam(":nombre_corto", $nombre_corto);
-                $stmt->bindParam(":precio", $precio);
-                $stmt->bindParam(":familia", $familia);
-                $stmt->bindParam(":descripcion", $descripcion);
-                $stmt->execute();
-                if ($stmt->rowCount() == 1) {
-                    $insertado = true;
-                    $mensajeAlerta = 'Se ha añadido el producto correctamente.';
-                } else {
-                    $mensajeAlerta = 'No se ha podido añadido el producto.';
+            if(isset($conexion)) {
+
+                try {
+                    $stmt = $conexion->prepare($consultaSQL);
+                    $stmt->bindParam(":nombre", $nombre);
+                    $stmt->bindParam(":nombre_corto", $nombre_corto);
+                    $stmt->bindParam(":precio", $precio);
+                    $stmt->bindParam(":familia", $familia);
+                    $stmt->bindParam(":descripcion", $descripcion);
+                    $stmt->execute();
+                    if ($stmt->rowCount() == 1) {
+                        $insertado = true;
+                        $mensajeAlerta = 'Se ha añadido el producto correctamente.';
+                    } else {
+                        $mensajeAlerta = 'No se ha podido añadido el producto.';
+                    }
+                    
+                } catch (PDOException $e) {
+                    $mensajeAlerta = miGestorDeErrores('ERROR SQL', null, $e->getCode());
+                    $error = true;
+                } catch (Throwable $e) {
+                    echo "HOLI";
+                    $mensajeAlerta = miGestorDeErrores(E_ERROR, null, $e->getCode());
+                    $error = true;
+                    $errorConexionBD = true;
+                } finally {
+                    // Cerrar conexiones
+                    $stmt = null;
+                    $conexion = null;
                 }
-                
-            } catch (PDOException $e) {
-                $mensajeAlerta = miGestorDeErrores('ERROR SQL', null, $e->getCode());
+
+            } else {
+                $mensajeAlerta = miGestorDeErrores(E_ERROR, null, $e->getCode());
                 $error = true;
-            } finally {
-                // Cerrar conexiones
-                $stmt = null;
-                $conexion = null;
+                $errorConexionBD = true;
             }
+            
 
         }
         
@@ -74,6 +89,7 @@
             $mensajeAlerta = miGestorDeErrores('ERROR SQL', null, $e->getCode());
             $error = true;
         } finally {
+            $stmt = null;
             $conexion = null;
         }
         
@@ -104,69 +120,71 @@
 
             <div id="liveAlertPlaceholder" class="row mt-2"></div>
 
-            <form method="POST" class="row mt-2">
+            <?php if(!$errorConexionBD) : ?>
+                <form method="POST" class="row mt-2">
 
-                <div class="col-6">
-                    <label class="form-label" for="nombre">Nombre</label>
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        name="nombre" 
-                        id="nombre" 
-                        pattern="\S?([\w-]+[.]?[\s]?)+"
-                        title="El nombre no puede quedar vacío, puede contener letras mayúsculas o minúsculas y números o puntos no consecutivos." 
-                        placeholder="Nombre"
-                        required>
-                </div>
+                    <div class="col-6">
+                        <label class="form-label" for="nombre">Nombre</label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            name="nombre" 
+                            id="nombre" 
+                            pattern="\S?([\w-]+[.]?[\s]?)+"
+                            title="El nombre no puede quedar vacío, puede contener letras mayúsculas o minúsculas y números o puntos no consecutivos." 
+                            placeholder="Nombre"
+                            required>
+                    </div>
 
-                <div class="col-6">
-                    <label class="form-label" for="nombre_corto">Nombre Corto</label>
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        name="nombre_corto" 
-                        id="nombre_corto" 
-                        placeholder="Nombre Corto"
-                        pattern="[A-Z]+[\d]*"
-                        minlength="3"
-                        title="Un código en mayúsculas que no empiece por un número" 
-                        required>
-                </div>
-                
-                <div class="col-6 mt-3">
-                    <label class="form-label" for="precio">Precio (€)</label>
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        name="precio" 
-                        id="precio" 
-                        pattern="([0-9]*[.])?[0-9]+"
-                        title="Un número con los decimales separados por un punto" 
-                        placeholder="Precio (€)" 
-                        required>
-                </div>
+                    <div class="col-6">
+                        <label class="form-label" for="nombre_corto">Nombre Corto</label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            name="nombre_corto" 
+                            id="nombre_corto" 
+                            placeholder="Nombre Corto"
+                            pattern="[A-Z]+[\d]*"
+                            minlength="3"
+                            title="Un código en mayúsculas que no empiece por un número" 
+                            required>
+                    </div>
+                    
+                    <div class="col-6 mt-3">
+                        <label class="form-label" for="precio">Precio (€)</label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            name="precio" 
+                            id="precio" 
+                            pattern="([0-9]*[.])?[0-9]+"
+                            title="Un número con los decimales separados por un punto" 
+                            placeholder="Precio (€)" 
+                            required>
+                    </div>
 
-                <div class="col-6 mt-3">
-                    <label class="form-label" for="familia">Familia</label>
-                    <?php mostrarSelectFamilias($mensajeAlerta, $error) ?>
-                </div>
+                    <div class="col-6 mt-3">
+                        <label class="form-label" for="familia">Familia</label>
+                        <?php mostrarSelectFamilias($mensajeAlerta, $error) ?>
+                    </div>
 
-                <div class="col-9 mt-3">
-                    <label class="form-label" for="descripcion">Descripción</label>
-                    <textarea 
-                        class="form-control" 
-                        name="descripcion" 
-                        id="descripcion" 
-                        required></textarea>
-                </div>
+                    <div class="col-9 mt-3">
+                        <label class="form-label" for="descripcion">Descripción</label>
+                        <textarea 
+                            class="form-control" 
+                            name="descripcion" 
+                            id="descripcion" 
+                            required></textarea>
+                    </div>
 
-                <div class="mt-3">
-                    <button type="submit" name="crear" class="btn btn-primary btn-lg">Crear</button>
-                    <button type="reset" class="btn btn-success btn-lg ms-3">Limpiar</button>
-                    <a href="listado.php"><button type="button" class="btn btn-info text-white btn-lg ms-3">Volver</button></a>
-                </div>
+                    <div class="mt-3">
+                        <button type="submit" name="crear" class="btn btn-primary btn-lg">Crear</button>
+                        <button type="reset" class="btn btn-success btn-lg ms-3">Limpiar</button>
+                        <a href="listado.php"><button type="button" class="btn btn-info text-white btn-lg ms-3">Volver</button></a>
+                    </div>
 
-            </form>
+                </form>
+            <?php endif ?>
 
         </div>
 
